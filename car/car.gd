2c3ea89e53fe30c78f607ab56a308c25de4b3236
mje819 <2260211@flourbluffschools.org>
2026-02-03 12:40:42 -0600

    tweak car's behavior wip

--- car/car.gd
+++ car/car.gd
@@ -11,3 +11,7 @@ func _physics_process(delta: float) -> void:
 	engine_force = -Input.get_axis("accelerate","brake") * force
 	#print(self.position)
 	steering = move_toward(steering, Input.get_axis("turn_right","turn_left") * 0.9,delta)
+	
+	# Maybe only for testing
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
