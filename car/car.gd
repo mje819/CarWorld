8ad662281439131d432fb8c8e8842f5f18e71f8d
mje819 <2260211@flourbluffschools.org>
2026-02-03 12:20:22 -0600

    make driving car

---
+++ car/car.gd
@@ -0,0 +1,13 @@
+extends VehicleBody3D
+
+@export var force := 50
+@export var braking_force := 50
+@export var max_steer = 0.9
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	engine_force = -Input.get_axis("accelerate","brake") * force
+	#print(self.position)
+	steering = move_toward(steering, Input.get_axis("turn_right","turn_left") * 0.9,delta)
