926d8533d3d5a024e78e2c272e3e75079fa982f4
mje819 <2260211@flourbluffschools.org>
2026-02-05 11:53:41 -0600

    add time trail start detector

--- car/car_manager.gd
+++ car/car_manager.gd
@@ -1,17 +1,20 @@
 extends Node
 
-@export var health : int :
+
+@export var max_health := 10
+var health : int :
 	set(new_health):
 		health = new_health
 		print(health)
 		#label changes?
 
 func _ready() -> void:
-	health = 10
+	health = max_health
 
 func _process(_delta: float) -> void:
 	# Maybe only for testing
-	if Input.is_action_pressed("reset"):
+	if Input.is_action_just_pressed("reset"):
+		health = max_health
 		get_tree().reload_current_scene()
 
 func take_damage() -> void:
