344134e0e75f1f8d16e323f602b092a2270dc919
mje819 <2260211@flourbluffschools.org>
2026-02-05 12:41:21 -0600

    create time trial timer and track wip

---
+++ Tracks/track.gd
@@ -0,0 +1,7 @@
+extends Path3D
+
+
+
+
+func _on_start_detector_body_entered(_body: Node3D) -> void:
+	print("start timer")
--- World/time_trial_start.gd
+++ World/time_trial_start.gd
@@ -1,6 +1,13 @@
 extends Area3D
 
+@export var track : PackedScene = null
+var has_track = false
 
-
-func _on_body_entered(body: Node3D) -> void:
-	print("start time trial")
+func _on_body_entered(_body: Node3D) -> void:
+	print("create track")
+	if !has_track:
+		var new_track = track.instantiate()
+		add_child(new_track)
+		new_track.global_position = global_position
+		has_track = true
+	# create a timer
