651debe54253635a040adb05919b8ca969f2db7a
mje819 <2260211@flourbluffschools.org>
2026-02-10 12:34:33 -0600

    add timer for the track

--- Tracks/track.gd
+++ Tracks/track.gd
@@ -1,7 +1,32 @@
 extends Path3D
+class_name Track
 
-
-
+@onready var timer: Timer = $Timer
+var time_elapsed : float = 0:
+	set(new_time):
+		time_elapsed = new_time
+		print(time_elapsed) # add ui updating
+var best_time : float
 
 func _on_start_detector_body_entered(_body: Node3D) -> void:
 	print("start timer")
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	time_elapsed += 0.1
+
+
+func _on_finish_detector_body_entered(_body: Node3D) -> void:
+	if !timer.is_stopped():
+		print("finished")
+		if best_time != 0:
+			if time_elapsed < best_time:
+				best_time = time_elapsed
+		else:
+			best_time = time_elapsed
+	
+		timer.stop()
+		time_elapsed = 0
+	
+		print("Best: " + str(best_time))
--- World/time_trial_start.gd
+++ World/time_trial_start.gd
@@ -1,13 +1,16 @@
 extends Area3D
 
-@export var track : PackedScene = null
+@export var track : Track
 var has_track = false
 
 func _on_body_entered(_body: Node3D) -> void:
 	print("create track")
 	if !has_track:
-		var new_track = track.instantiate()
-		add_child(new_track)
-		new_track.global_position = global_position
+		#var new_track = track.instantiate()
+		#add_child(new_track)
+		#new_track.position = global_position
+		track.visible = true
 		has_track = true
-	# create a timer
+		for detector in get_tree().get_nodes_in_group("Start and Finish Detectors"):
+			detector.monitoring = true
+		# create a timer
