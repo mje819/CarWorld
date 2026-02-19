b677fad5ee0c3c69f08d5765a46ad77732dbf39d
mje819 <2260211@flourbluffschools.org>
2026-02-19 12:27:16 -0600

    make car teleport to track start

--- World/time_trial_start.gd
+++ World/time_trial_start.gd
@@ -1,7 +1,12 @@
 extends Area3D
 
 @export var track : Track
+var start_point : Marker3D
 var has_track = false
+
+func _ready() -> void:
+	if track != null:
+		start_point = track.start_point
 
 func _on_body_entered(_body: Node3D) -> void:
 	print("create track")
@@ -13,4 +18,6 @@ func _on_body_entered(_body: Node3D) -> void:
 	has_track = true
 	for detector in get_tree().get_nodes_in_group("Start and Finish Detectors"):
 		detector.monitoring = true
-	# create a timer
+	_body.global_position = start_point.global_position
+	_body.global_rotation = start_point.global_rotation
+	_body.engine_force = 0
