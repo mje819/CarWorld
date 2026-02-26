9c0704ca5ea07b4125805ce8d0b8bf4eeb230d20
mje819 <2260211@flourbluffschools.org>
2026-02-26 12:20:37 -0600

    add engine sounds

--- car/car.gd
+++ car/car.gd
@@ -7,6 +7,7 @@ class_name Car
 @export var health := 10
 #@onready var camera_3d: Camera3D = $Camera3D
 @onready var camera_holder: Node3D = $CameraHolder
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 
 func _ready() -> void:
@@ -17,7 +18,10 @@ func _physics_process(delta: float) -> void:
 	#print(self.position)
 	steering = move_toward(steering, Input.get_axis("turn_right","turn_left") * max_steer,delta)
 	
-	
+	if engine_force != 0 and !audio_stream_player_3d.playing:
+		audio_stream_player_3d.play()
+	if engine_force == 0:
+		audio_stream_player_3d.stop()
 	
 	camera_holder.global_position = global_position
 	camera_holder.rotation.y = rotation.y
